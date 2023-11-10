d32785c0d7fa20507364f474550d6709aaa61bf7
Wontae Lee <maxandjava@gmail.com>
2023-11-10 18:40:09 +0900

    modified kernel

--- dataset/gaussian_kernel.py
+++ dataset/gaussian_kernel.py
@@ -4,32 +4,36 @@ import numpy as np
 
 class Kernel:
 
-    def __init__(self, kernel_points: np.array, geometry_points: np.array, h: float):
+    def __init__(self, kernel_points: np.array, geometry_points: np.array, h: float, save_path: str = None):
         self.kernel_points = kernel_points
         self.geometry_points = geometry_points
         self.h = h
-        self.sigma = 1/ (torch.pi**(3/2) * h**3)
+        self.sigma = 1 / (torch.pi ** (3 / 2) * h ** 3)
+        self.data = self.__compute()
+        if save_path:
+            np.save(save_path, self.data)
 
-    def compute(self):
-        test_result = self.gaussian(self.kernel_points[0], self.geometry_points[0])
-        print(test_result)
+    def __compute(self):
+
+        data_chunk = []
+        for pcd1 in self.kernel_points:
+            for pcd2 in self.geometry_points:
+                data_chunk.append([self.gaussian(pcd1, pcd2)])
+
+        data_chunk = np.array(data_chunk, dtype=object)
+
+        return data_chunk
 
     def gaussian(self, point_cloud1: np.array, point_cloud2: np.array):
         pcd_torch1 = torch.from_numpy(point_cloud1).to('cuda').unsqueeze(0)
         pcd_torch2 = torch.from_numpy(point_cloud2).to('cuda').unsqueeze(0).permute(1, 0, 2)
 
         differences = pcd_torch1 - pcd_torch2
-        distances = torch.sqrt(torch.sum(differences ** 2, dim=2))
+        result = self.sigma * torch.exp((self.h / torch.sqrt(torch.sum(differences ** 2, dim=2))) ** 2)
+        result = result.to_numpy()
 
-        q_term = self.h / distances
-        e_term = torch.exp(-q_term ** 2)
-        return self.sigma * e_term
+        return result
 
 
 if __name__ == "__main__":
-    sampled_points = np.load('../dataset/geometry/grid_sampled.npy', allow_pickle=True)
-    kernel_points = np.load('../dataset/kernel_points/kernel_points.npy', allow_pickle=True)
-
-    kernel = Kernel(kernel_points, sampled_points, 0.1)
-    check = kernel.gaussian(kernel_points[0], sampled_points[0])
-    print(check)
+    pass
