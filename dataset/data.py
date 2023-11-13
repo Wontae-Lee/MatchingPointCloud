fe8cca01d87eb70f8205c2d599389c70194ec6bc
Wontae Lee <maxandjava@gmail.com>
2023-11-13 16:19:30 +0900

    modified kernel

--- dataset/data.py
+++ dataset/data.py
@@ -1,38 +1,41 @@
-import torch
 import numpy as np
 import open3d as o3d
 
-torch.manual_seed(0)
-torch.cuda.manual_seed_all(0)
-np.random.seed(0)
 
+class Data:
+    def __init__(self,
+                 raw_path="./geometry/raw.stl",
+                 number_of_points=500000,
+                 splicing_height=0.05,
+                 unit="mm",
+                 save_raw=None,
+                 save_implant=None,
+                 save_bone=None):
 
-class NIMSqtt:
-    def __init__(self, raw_path="./geometry/raw.stl"):
-        self.raw = self.stl_to_point_cloud(raw_path)
-        self.implant = self.raw[np.where(self.raw[:, 2] >= 0.045)]
-        self.bone = self.raw[np.where(self.raw[:, 2] < 0.045)]
+        self.raw = self.stl_to_point_cloud(raw_path, number_of_points)
+        self.implant = self.raw[np.where(self.raw[:, 2] >= splicing_height)]
+        self.bone = self.raw[np.where(self.raw[:, 2] < splicing_height)]
+        if unit == "mm":
+            self.raw *= 1000
+            self.implant *= 1000
+            self.bone *= 1000
 
-        self.save_point_cloud(self.raw, "geometry/raw.npy")
-        self.save_point_cloud(self.implant, "geometry/implant.npy")
-        self.save_point_cloud(self.bone, "geometry/bone.npy")
+        if save_raw is not None:
+            self.save_point_cloud(self.raw, save_raw)
+        if save_implant is not None:
+            self.save_point_cloud(self.implant, save_implant)
+        if save_bone is not None:
+            self.save_point_cloud(self.bone, save_bone)
 
     @staticmethod
-    def stl_to_point_cloud(stl_filename, numpoints=500000):
+    def stl_to_point_cloud(raw_path, numpoints=500000):
         # Load the STL file into a triangle mesh
-        triangle_mesh = o3d.io.read_triangle_mesh(stl_filename)
+        triangle_mesh = o3d.io.read_triangle_mesh(raw_path)
         triangle_mesh.compute_vertex_normals()
 
         # Sample points from the triangle mesh
         point_cloud = triangle_mesh.sample_points_uniformly(number_of_points=numpoints)
         return np.asarray(point_cloud.points)
-
-    @staticmethod
-    def visualization(point_cloud):
-        raw_pcd = o3d.geometry.PointCloud()
-        raw_pcd.points = o3d.utility.Vector3dVector(point_cloud)
-
-        o3d.visualization.draw_geometries([raw_pcd])
 
     @classmethod
     def save_point_cloud(cls, point_cloud, path):
@@ -40,4 +43,4 @@ class NIMSqtt:
 
 
 if __name__ == "__main__":
-    data = NIMSqtt()
+    pass
---
+++ dataset/sampling.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+class GridSampling:
+
+    def __init__(self, point_cloud_path: str, grid_size: np.array, save_sample_path: str = None,
+                 save_center_path: str = None):
+        # Load the point cloud
+        self.point_cloud = np.load(point_cloud_path)
+
+        # define the grid size
+        self.grid_size = grid_size
+
+        # Define the grid indices to which each point belongs
+        self.grid_indices = (self.point_cloud // grid_size).astype(int)
+
+        # Get the unique grid indices
+        self.unique_grid_indices = np.unique(self.grid_indices, axis=0)
+
+        # Initialize the sampled point cloud
+        self.sampled_point_cloud = self.__sampling()
+
+        # Save the sampled point cloud
+        if save_sample_path is not None:
+            self.save_sampled_point_cloud(save_sample_path)
+        if save_center_path is not None:
+            self.save_grid_indices_point_cloud(save_center_path)
+
+    def __sampling(self):
+        sampled_point_cloud = []
+
+        for i in range(self.unique_grid_indices.shape[0]):
+            # Create a mask for the current grid cell
+            mask = np.zeros(self.point_cloud.shape[0], dtype=bool)
+            mask[np.where(np.all(self.unique_grid_indices[i] == self.grid_indices, axis=1))] = True
+
+            # Get the points in the current grid cell
+            sampled_point_cloud.append(self.point_cloud[mask])
+
+        # Convert the list to a numpy array
+        sampled_point_cloud = np.array(sampled_point_cloud, dtype=object)
+
+        return sampled_point_cloud
+
+    def save_grid_indices_point_cloud(self, path: str):
+        # Convert the grid indices to a point cloud
+        grid_indices_point_cloud = self.unique_grid_indices * self.grid_size + self.grid_size / 2
+
+        assert grid_indices_point_cloud.shape[-1] == 3, "The grid indices point cloud must be 3D"
+        assert len(grid_indices_point_cloud.shape) == 2, "The shape of the grid indices point cloud must be (N, 3)"
+
+        np.save(path, grid_indices_point_cloud)
+
+    def save_sampled_point_cloud(self, path: str):
+        # Save the sampled point cloud
+        np.save(path, self.sampled_point_cloud, allow_pickle=True)
+
+
+if __name__ == "__main__":
+    pass
